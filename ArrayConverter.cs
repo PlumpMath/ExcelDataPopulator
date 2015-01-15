43a8c79fdd82512c57d1dea83d64bec24b9123f5
Bembeng Arifin <bembengarifin@gmail.com>
2015-01-16 03:11:39 +0800

    added the missing files

---
+++ ArrayConverter.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelDataPopulator
+{
+    class ArrayConverter
+    {
+        internal static string[,] ConvertTo2DStringArrayStatically(IList<SampleObject> items)
+        {
+            var stringArray = new string[items.Count + 1, 9];
+            // headers
+            stringArray[0, 0] = "RowId";
+            stringArray[0, 1] = "A";
+            stringArray[0, 2] = "B";
+            stringArray[0, 3] = "C";
+            stringArray[0, 4] = "D";
+            stringArray[0, 5] = "E";
+            stringArray[0, 6] = "Qty";
+            stringArray[0, 7] = "Price";
+            stringArray[0, 8] = "Amount";
+
+            // data
+            for (int i = 0; i < items.Count; i++)
+            {
+                var item = items[i];
+                var arrayIndex = i + 1;
+                stringArray[arrayIndex, 0] = item.RowId.ToString();
+                stringArray[arrayIndex, 1] = item.A;
+                stringArray[arrayIndex, 2] = item.B;
+                stringArray[arrayIndex, 3] = item.C;
+                stringArray[arrayIndex, 4] = item.D;
+                stringArray[arrayIndex, 5] = item.E;
+                stringArray[arrayIndex, 6] = item.Qty.ToString();
+                stringArray[arrayIndex, 7] = item.Price.ToString();
+                stringArray[arrayIndex, 8] = item.Amount.ToString();
+            }
+            return stringArray;
+        }
+
+        internal static string[,] ConvertTo2DStringArrayWithReflection<T>(IList<T> items)
+        {
+            var props = typeof(T).GetProperties();
+            var stringArray = new string[items.Count + 1, props.Length];
+            // headers
+            for (int iProp = 0; iProp < props.Length; iProp++)
+            {
+                stringArray[0, iProp] = props[iProp].Name;
+            }
+
+            // data
+            for (int i = 0; i < items.Count; i++)
+            {
+                var item = items[i];
+                var arrayIndex = i + 1;
+                for (int iProp = 0; iProp < props.Length; iProp++)
+                {
+                    var val = props[iProp].GetValue(item);
+                    if (val != null)
+                        stringArray[arrayIndex, iProp] = val.ToString();
+                }
+            }
+            return stringArray;
+        }
+
+        internal static Func<IList<T>, string[,]> ConvertTo2DStringArrayWithExpressionTree<T>()
+        {
+            var t = typeof(T);
+            var tProperties = t.GetProperties();
+
+            var statements = new List<Expression>();
+
+            var listInputParameter = Expression.Parameter(typeof(IList<T>));
+            var itemPropertiesCount = Expression.Constant(tProperties.Length);
+            var rowCountExpression = Expression.Property(listInputParameter, typeof(ICollection<T>), "Count");
+            var rowCountPlusHeaderExpression = Expression.Add(rowCountExpression, Expression.Constant(1));
+            var itemIndexVariable = Expression.Variable(typeof(int));
+            var arrayIndexVariable = Expression.Variable(typeof(int));
+            var currentItem = Expression.Variable(t);
+
+            statements.Add(Expression.Call(typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string), typeof(string) })
+                            , Expression.Constant("Converting {0} rows with expression tree")
+                            , Expression.Call(rowCountExpression, typeof(object).GetMethod("ToString"))));
+
+            // Initialize string array based on the items row count and the 
+            var stringArrayVariable = Expression.Variable(typeof(string[,]));
+            var newArrayExpression = Expression.NewArrayBounds(typeof(string), rowCountPlusHeaderExpression, itemPropertiesCount);
+            var initializeArray = Expression.Assign(stringArrayVariable, newArrayExpression);
+            statements.Add(initializeArray);
+
+            // headers
+            for (int pIndex = 0; pIndex < tProperties.Length; pIndex++)
+            {
+                statements.Add(Expression.Assign(Expression.ArrayAccess(stringArrayVariable, new List<Expression> { Expression.Constant(0), Expression.Constant(pIndex) }), Expression.Constant(tProperties[pIndex].Name)));
+            }
+
+            // Prepare item assignments here as it requires loop on the properties which can't be done inline
+            var itemStatements = new List<Expression>();
+            itemStatements.Add(Expression.Assign(currentItem, Expression.Property(listInputParameter, "Item", itemIndexVariable)));
+            itemStatements.Add(Expression.Assign(arrayIndexVariable, Expression.Add(itemIndexVariable, Expression.Constant(1))));
+
+            for (int pIndex = 0; pIndex < tProperties.Length; pIndex++)
+            {
+                var prop = tProperties[pIndex];
+                var propExpression = Expression.Property(currentItem, prop);
+
+                var callConvertExpression = Expression.Call(typeof(System.Convert).GetMethod("ToString", new Type[] { prop.PropertyType }), propExpression);
+                // boxing is required here for excel to bind the data type correctly, converting all data to string will cause excel to treat numbers as text
+                //var callConvertExpression = Expression.Convert(propExpression, typeof(object));
+
+                itemStatements.Add(Expression.Assign(Expression.ArrayAccess(stringArrayVariable, new List<Expression> { arrayIndexVariable, Expression.Constant(pIndex) }), callConvertExpression));
+            }
+            itemStatements.Add(Expression.PostIncrementAssign(itemIndexVariable));
+
+            // iterate the items
+            var label = Expression.Label();
+            var forLoopBody = Expression.Block(
+                new[] { itemIndexVariable }, // local variable
+                Expression.Assign(itemIndexVariable, Expression.Constant(0)), // initialize with 0
+                Expression.Loop(
+                    Expression.IfThenElse(
+                        Expression.LessThan(itemIndexVariable, rowCountExpression), // test row count + header
+                        Expression.Block(new[] { currentItem, arrayIndexVariable }, itemStatements), // execute if true
+                        Expression.Break(label)) // execute if false
+                    , label));
+            statements.Add(forLoopBody);
+
+            // return statement
+            statements.Add(stringArrayVariable);
+
+            var body = Expression.Block(stringArrayVariable.Type, new[] { stringArrayVariable }, statements.ToArray());
+            var compiled = Expression.Lambda<Func<IList<T>, string[,]>>(body, listInputParameter).Compile();
+            return compiled;
+        }
+
+        internal static object[,] ConvertTo2DObjectArrayStatically(IList<SampleObject> items)
+        {
+            var stringArray = new object[items.Count + 1, 9];
+            // headers
+            stringArray[0, 0] = "RowId";
+            stringArray[0, 1] = "A";
+            stringArray[0, 2] = "B";
+            stringArray[0, 3] = "C";
+            stringArray[0, 4] = "D";
+            stringArray[0, 5] = "E";
+            stringArray[0, 6] = "Qty";
+            stringArray[0, 7] = "Price";
+            stringArray[0, 8] = "Amount";
+
+            // data
+            for (int i = 0; i < items.Count; i++)
+            {
+                var item = items[i];
+                var arrayIndex = i + 1;
+                stringArray[arrayIndex, 0] = item.RowId;
+                stringArray[arrayIndex, 1] = item.A;
+                stringArray[arrayIndex, 2] = item.B;
+                stringArray[arrayIndex, 3] = item.C;
+                stringArray[arrayIndex, 4] = item.D;
+                stringArray[arrayIndex, 5] = item.E;
+                stringArray[arrayIndex, 6] = item.Qty;
+                stringArray[arrayIndex, 7] = item.Price;
+                stringArray[arrayIndex, 8] = item.Amount;
+            }
+            return stringArray;
+        }
+
+        internal static object[,] ConvertTo2DObjectArrayWithReflection<T>(IList<T> items)
+        {
+            var props = typeof(T).GetProperties();
+            var stringArray = new object[items.Count + 1, props.Length];
+            // headers
+            for (int iProp = 0; iProp < props.Length; iProp++)
+            {
+                stringArray[0, iProp] = props[iProp].Name;
+            }
+
+            // data
+            for (int i = 0; i < items.Count; i++)
+            {
+                var item = items[i];
+                var arrayIndex = i + 1;
+                for (int iProp = 0; iProp < props.Length; iProp++)
+                {
+                    var val = props[iProp].GetValue(item);
+                    if (val != null)
+                        stringArray[arrayIndex, iProp] = val;
+                }
+            }
+            return stringArray;
+        }
+
+        internal static Func<IList<T>, object[,]> ConvertTo2DObjectArrayWithExpressionTree<T>()
+        {
+            var t = typeof(T);
+            var tProperties = t.GetProperties();
+
+            var statements = new List<Expression>();
+
+            var listInputParameter = Expression.Parameter(typeof(IList<T>));
+            var itemPropertiesCount = Expression.Constant(tProperties.Length);
+            var rowCountExpression = Expression.Property(listInputParameter, typeof(ICollection<T>), "Count");
+            var rowCountPlusHeaderExpression = Expression.Add(rowCountExpression, Expression.Constant(1));
+            var itemIndexVariable = Expression.Variable(typeof(int));
+            var arrayIndexVariable = Expression.Variable(typeof(int));
+            var currentItem = Expression.Variable(t);
+
+            statements.Add(Expression.Call(typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string), typeof(string) })
+                            , Expression.Constant("Converting {0} rows with expression tree")
+                            , Expression.Call(rowCountExpression, typeof(object).GetMethod("ToString"))));
+
+            // Initialize string array based on the items row count and the 
+            var stringArrayVariable = Expression.Variable(typeof(object[,]));
+            var newArrayExpression = Expression.NewArrayBounds(typeof(object), rowCountPlusHeaderExpression, itemPropertiesCount);
+            var initializeArray = Expression.Assign(stringArrayVariable, newArrayExpression);
+            statements.Add(initializeArray);
+
+            // headers
+            for (int pIndex = 0; pIndex < tProperties.Length; pIndex++)
+            {
+                statements.Add(Expression.Assign(Expression.ArrayAccess(stringArrayVariable, new List<Expression> { Expression.Constant(0), Expression.Constant(pIndex) }), Expression.Constant(tProperties[pIndex].Name)));
+            }
+
+            // Prepare item assignments here as it requires loop on the properties which can't be done inline
+            var itemStatements = new List<Expression>();
+            itemStatements.Add(Expression.Assign(currentItem, Expression.Property(listInputParameter, "Item", itemIndexVariable)));
+            itemStatements.Add(Expression.Assign(arrayIndexVariable, Expression.Add(itemIndexVariable, Expression.Constant(1))));
+
+            for (int pIndex = 0; pIndex < tProperties.Length; pIndex++)
+            {
+                var prop = tProperties[pIndex];
+                var propExpression = Expression.Property(currentItem, prop);
+
+                //var callConvertExpression = Expression.Call(typeof(System.Convert).GetMethod("ToString", new Type[] { prop.PropertyType }), propExpression);
+                // boxing is required here for excel to bind the data type correctly, converting all data to string will cause excel to treat numbers as text
+                var callConvertExpression = Expression.Convert(propExpression, typeof(object));
+
+                itemStatements.Add(Expression.Assign(Expression.ArrayAccess(stringArrayVariable, new List<Expression> { arrayIndexVariable, Expression.Constant(pIndex) }), callConvertExpression));
+            }
+            itemStatements.Add(Expression.PostIncrementAssign(itemIndexVariable));
+
+            // iterate the items
+            var label = Expression.Label();
+            var forLoopBody = Expression.Block(
+                new[] { itemIndexVariable }, // local variable
+                Expression.Assign(itemIndexVariable, Expression.Constant(0)), // initialize with 0
+                Expression.Loop(
+                    Expression.IfThenElse(
+                        Expression.LessThan(itemIndexVariable, rowCountExpression), // test row count + header
+                        Expression.Block(new[] { currentItem, arrayIndexVariable }, itemStatements), // execute if true
+                        Expression.Break(label)) // execute if false
+                    , label));
+            statements.Add(forLoopBody);
+
+            // return statement
+            statements.Add(stringArrayVariable);
+
+            var body = Expression.Block(stringArrayVariable.Type, new[] { stringArrayVariable }, statements.ToArray());
+            var compiled = Expression.Lambda<Func<IList<T>, object[,]>>(body, listInputParameter).Compile();
+            return compiled;
+        }
+    }
+}
---
+++ Examples.cs
@@ -0,0 +1,278 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace ExcelDataPopulator
+{
+    class Examples
+    {
+        public static void RunStringArrayConverterPerformance()
+        {
+            var sw = Stopwatch.StartNew();
+
+            var items = Enumerable.Range(1, 1000000).Select(i => new SampleObject { A = "A" + i, B = "B" + i, C = "C" + i, D = "D" + i, E = "E" + i }).ToList();
+            string[,] stringArray = null;
+            object[,] objectArray = null;
+
+            LogHelper.LogFormat("Initialized {0} rows, {1} ms", items.Count.ToString(), sw.ElapsedMilliseconds.ToString());
+            sw.Restart();
+
+            for (int i = 0; i < 10; i++)
+                stringArray = ArrayConverter.ConvertTo2DStringArrayStatically(items);
+
+            LogHelper.LogFormat("Convert with string array statically is completed in {0} ms", sw.ElapsedMilliseconds.ToString());
+            sw.Restart();
+
+            //for (int i = 0; i < 10; i++)
+            //    stringArray = ArrayConverter.ConvertTo2DStringArrayWithReflection(items);
+
+            //LogHelper.LogFormat("Convert with string array reflection is completed in {0} ms", sw.ElapsedMilliseconds.ToString());
+            //sw.Restart();
+
+            //for (int i = 0; i < 10; i++)
+            //    stringArray = ArrayConverter.ConvertTo2DStringArrayWithExpressionTree<SampleObject>()(items);
+
+            //LogHelper.LogFormat("Convert with string array expression tree is completed in {0} ms", sw.ElapsedMilliseconds.ToString());
+            //sw.Restart();
+
+            for (int i = 0; i < 10; i++)
+                objectArray = ArrayConverter.ConvertTo2DObjectArrayStatically(items);
+
+            LogHelper.LogFormat("Convert with statically is completed in {0} ms", sw.ElapsedMilliseconds.ToString());
+            sw.Restart();
+
+            //for (int i = 0; i < 10; i++)
+            //    objectArray = ArrayConverter.ConvertTo2DObjectArrayWithReflection(items);
+
+            //LogHelper.LogFormat("Convert with reflection is completed in {0} ms", sw.ElapsedMilliseconds.ToString());
+            //sw.Restart();
+
+            //for (int i = 0; i < 10; i++)
+            //    objectArray = ArrayConverter.ConvertTo2DObjectArrayWithExpressionTree<SampleObject>()(items);
+
+            //LogHelper.LogFormat("Convert with expression tree is completed in {0} ms", sw.ElapsedMilliseconds.ToString());
+            //sw.Restart();
+
+            //new NumberGenerator().Generate();
+            //}
+            //Console.WriteLine(stringArray[items.Count - 1, 0]);
+
+            //RunPopulate();
+
+            //var obj = new SampleObject { A = "StringA", B = "StringB", C = "StringC", D = "StringD", E = "StringE" };
+            //PrintObjectStatically(obj);
+            //PrintObjectWithReflection(obj);
+            //PrintObjectWithExpressionTree(obj);
+
+            Console.WriteLine("Finished in {0} ms", sw.ElapsedMilliseconds);
+            sw.Stop();
+            Console.Read();
+        }
+
+        public static void RunPopulate()
+        {
+            var rnd = new Random();
+
+            // prepare data source
+            var items = Enumerable.Range(1, 1000000)
+                                  .Select(i => new SampleObject
+                                  {
+                                      RowId = i,
+                                      A = "A" + rnd.Next(1, 5),
+                                      B = "B" + rnd.Next(1, 5),
+                                      C = "C" + rnd.Next(1, 5),
+                                      D = "D" + rnd.Next(1, 5),
+                                      E = "E" + rnd.Next(1, 5),
+                                      Qty = Convert.ToDecimal(rnd.NextDouble() * 100),
+                                      Price = Convert.ToDecimal(rnd.NextDouble() * 1000)
+                                  }).ToList();
+
+            string filePath = null;
+
+            // uncomment 1 option below to verify the performance and results
+            //ExcelAPIHelper.CreateNew(WaysToAssignValues);
+            //ExcelAPIHelper.CreateNew(ws => PopulateItemsWithSingleCellMethod(ws, items));
+            //ExcelAPIHelper.CreateNew(ws => PopulateItemsWithMultiCellsOneRowMethod(ws, items));
+            //filePath = ExcelAPIHelper.CreateNew(ws => PopulateItemsWithMultiCellsAndRowsMethod(ws, items));
+            //filePath = ExcelAPIHelper.CreateNew(ws => PopulateItemsWithMultiCellsAndRowsMethodWithReflection(ws, items));
+            filePath = ExcelAPIHelper.CreateNew(2, (b, sheets) => PopulateItemsWithMultiCellsAndRowsMethodWithExpressionTree(b, sheets, items));
+
+            if (!string.IsNullOrWhiteSpace(filePath))
+                Process.Start(filePath);
+            Console.Read();
+
+            // clean up
+            //File.Delete(filePath);
+        }
+
+        static void WaysToAssignValues(Worksheet worksheet)
+        {
+            LogHelper.LogFormat("Ways to populate values into excel");
+            // DO NOT do this as it will create memory leak (excel stays as background process, visible in task manager)
+            // best pratice is to hold the object into a variable then call dispose (release com object)
+            //ws.get_Range("C5").Value = "ABC";
+
+            var r1 = worksheet.get_Range("A1"); // single cell
+            r1.Value = "A1";
+            ExcelAPIHelper.DisposeCOMObject(r1);
+
+            var r2 = worksheet.get_Range("A2:C2"); // multi cells in 1 row
+            r2.Value = new[] { "A2", "B2", "C2" };
+            ExcelAPIHelper.DisposeCOMObject(r2);
+
+            var r3 = worksheet.get_Range("A3:C4"); // multi cells/rows
+            r3.Value = new string[,] { { "A3", "B3", "C3" }, { "A4", "B4", "C4" } }; ;
+            ExcelAPIHelper.DisposeCOMObject(r3);
+        }
+
+        static void PopulateItemsWithSingleCellMethod(Worksheet worksheet, IList<SampleObject> items)
+        {
+            LogHelper.LogFormat("Start populating {0} rows using single cell method", items.Count.ToString());
+            for (int i = 0; i < items.Count; i++)
+            {
+                var item = items[i];
+                var rowIndex = i + 1;
+                var r1 = worksheet.get_Range("A" + rowIndex); r1.Value = item.A; ExcelAPIHelper.DisposeCOMObject(r1);
+                var r2 = worksheet.get_Range("B" + rowIndex); r2.Value = item.B; ExcelAPIHelper.DisposeCOMObject(r2);
+                var r3 = worksheet.get_Range("C" + rowIndex); r3.Value = item.C; ExcelAPIHelper.DisposeCOMObject(r3);
+                var r4 = worksheet.get_Range("D" + rowIndex); r4.Value = item.D; ExcelAPIHelper.DisposeCOMObject(r4);
+                var r5 = worksheet.get_Range("E" + rowIndex); r5.Value = item.E; ExcelAPIHelper.DisposeCOMObject(r5);
+            }
+        }
+
+        static void PopulateItemsWithMultiCellsOneRowMethod(Worksheet worksheet, IList<SampleObject> items)
+        {
+            LogHelper.LogFormat("Start populating {0} rows using multi cells (per 1 row) method", items.Count.ToString());
+            for (int i = 0; i < items.Count; i++)
+            {
+                var item = items[i];
+                var rowIndex = i + 1;
+                var r = worksheet.get_Range(string.Format("A{0}:E{0}", rowIndex));
+                r.Value = new[] { item.A, item.B, item.C, item.D, item.E };
+                ExcelAPIHelper.DisposeCOMObject(r);
+            }
+        }
+
+        static void PopulateItemsWithMultiCellsAndRowsMethod(Worksheet worksheet, IList<SampleObject> items)
+        {
+            LogHelper.LogFormat("Start populating {0} rows using multi cells and rows method", items.Count.ToString());
+
+            var stringArray = ArrayConverter.ConvertTo2DObjectArrayStatically(items);
+
+            LogHelper.LogFormat("Completed converting the data into string array");
+
+            var r = worksheet.get_Range(string.Format("A{0}:E{1}", 1, items.Count + 1));
+            r.Value = stringArray;
+            ExcelAPIHelper.DisposeCOMObject(r);
+        }
+
+        static void PopulateItemsWithMultiCellsAndRowsMethodWithReflection(Worksheet worksheet, IList<SampleObject> items)
+        {
+            LogHelper.LogFormat("Start populating {0} rows using multi cells and rows method with reflection", items.Count.ToString());
+
+            var stringArray = ArrayConverter.ConvertTo2DObjectArrayWithReflection(items);
+
+            LogHelper.LogFormat("Completed converting the data into string array");
+
+            var r = worksheet.get_Range(string.Format("A{0}:E{1}", 1, items.Count + 1));
+            r.Value = stringArray;
+            ExcelAPIHelper.DisposeCOMObject(r);
+        }
+
+        static void PopulateItemsWithMultiCellsAndRowsMethodWithExpressionTree(Workbook book, IList<Worksheet> worksheets, IList<SampleObject> items)
+        {
+            LogHelper.LogFormat("Start populating {0} rows using multi cells and rows method with expression tree", items.Count.ToString());
+            var objectArray = ArrayConverter.ConvertTo2DObjectArrayWithExpressionTree<SampleObject>()(items);
+            LogHelper.LogFormat("Completed converting the data into string array");
+
+            var dataRange = string.Format("{0}{1}:{2}{3}", ExcelColumnNames.GetColumnName(0), 1, ExcelColumnNames.GetColumnName(objectArray.GetUpperBound(1)), items.Count + 1);
+            var r = worksheets[0].get_Range(dataRange);
+            r.Value = objectArray;
+
+            var pivotCaches = book.PivotCaches();
+            var pivotCache = pivotCaches.Create(XlPivotTableSourceType.xlDatabase, string.Format("RawData!{0}", dataRange));
+            //var pivotCache = pivotCaches.Create(XlPivotTableSourceType.xlDatabase, r);
+
+            PivotTables pivotTables = worksheets[1].PivotTables() as PivotTables;
+            var pivotTable = pivotTables.Add(pivotCache, worksheets[1].get_Range("A1"), "PivotTable1") as PivotTable;
+
+            var fieldA = pivotTable.PivotFields("A") as PivotField;
+            fieldA.Orientation = XlPivotFieldOrientation.xlRowField;
+
+            var fieldB = pivotTable.PivotFields("B") as PivotField;
+            fieldB.Orientation = XlPivotFieldOrientation.xlHidden;
+
+            var fieldD = pivotTable.PivotFields("D") as PivotField;
+            fieldD.Orientation = XlPivotFieldOrientation.xlPageField;
+
+            var fieldE = pivotTable.PivotFields("E") as PivotField;
+            fieldE.Orientation = XlPivotFieldOrientation.xlColumnField;
+
+            var fieldC = pivotTable.PivotFields("C") as PivotField;
+            fieldC.Orientation = XlPivotFieldOrientation.xlDataField;
+            fieldC.Function = XlConsolidationFunction.xlCount;
+
+            pivotTable.AddDataField(pivotTable.PivotFields("Qty"), "Quantity", XlConsolidationFunction.xlSum);
+            pivotTable.AddDataField(pivotTable.PivotFields("Amount"), "SubTotal", XlConsolidationFunction.xlSum);
+            pivotTable.AddDataField(pivotTable.PivotFields("Price"), "Price Average", XlConsolidationFunction.xlSum);
+
+            ExcelAPIHelper.DisposeCOMObject(r);
+        }
+
+        static void PrintObjectStatically(SampleObject obj)
+        {
+            Console.WriteLine("PrintObjectStatically");
+            Console.WriteLine("A:" + obj.A);
+            Console.WriteLine("B:" + obj.B);
+            Console.WriteLine("C:" + obj.C);
+            Console.WriteLine("D:" + obj.D);
+            Console.WriteLine("E:" + obj.E);
+        }
+
+        static void PrintObjectWithReflection<T>(T obj)
+        {
+            Console.WriteLine("PrintObjectWithReflection");
+            var t = obj.GetType();
+
+            foreach (var p in t.GetProperties())
+            {
+                Console.WriteLine(p.Name + ":" + p.GetValue(obj));
+            }
+        }
+
+        static void PrintObjectWithExpressionTree(SampleObject obj)
+        {
+            Console.WriteLine("PrintObjectWithExpressionTree");
+            var printer = PrintStringValues<SampleObject>();
+
+            printer(obj);
+        }
+
+        static Action<T> PrintStringValues<T>()
+        {
+            var t = typeof(T);
+
+            var statements = new List<Expression>();
+            ParameterExpression instanceParameter = Expression.Parameter(t);
+            statements.Add(instanceParameter);
+
+            foreach (var p in t.GetProperties())
+            {
+                var propExpression = Expression.Property(instanceParameter, p);
+                MethodCallExpression mCall = Expression.Call(typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) }), propExpression);
+                statements.Add(mCall);
+            }
+
+            var body = Expression.Block(statements.ToArray());
+            var compiled = Expression.Lambda<Action<T>>(body, instanceParameter).Compile();
+            return compiled;
+        }
+
+    }
+}
